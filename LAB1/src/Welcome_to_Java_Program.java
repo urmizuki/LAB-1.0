4a1dd38375ac5f9983ed762c9f09807312b9111b
UPMUSER <UPMUSER@10.6.125.41>
2025-10-15 10:44:04 +0800

    First Lab

--- LAB1/src/Welcome_to_Java_Program.java
+++ LAB1/src/Welcome_to_Java_Program.java
@@ -2,6 +2,7 @@
 public class Welcome_to_Java_Program {
 	public static void main(String[] args) {
 		System.out.println("Welcome");
+		System.out.println("");
 		System.out.println("to");
 		System.out.println("Java");
 		System.out.println("Programming!");
