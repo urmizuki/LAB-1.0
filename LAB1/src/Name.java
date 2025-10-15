56f53919ad6dcc90b329c85152e99afb6a8d7630
UPMUSER <UPMUSER@10.6.125.41>
2025-10-15 10:24:36 +0800

    First Lab

---
+++ LAB1/src/Name.java
@@ -0,0 +1,9 @@
+
+public class Name {
+	public static void main(String[] args) {
+		System.out.println("ALIF SAFWAN BIN SAIFUL ADLI");
+		System.out.println("BC252095");
+		System.out.println("132 MUKIM I, KAMPUNG PERLIS, 11000 BALIK PULAU, PULAU PINANG.");
+	}
+
+}
