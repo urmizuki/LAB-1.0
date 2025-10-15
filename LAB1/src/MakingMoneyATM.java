70af5a964a8b74fa5db1cc4337f7e4c76275c201
UPMUSER <UPMUSER@10.6.125.41>
2025-10-15 10:13:10 +0800

    First Lab

--- LAB1/src/MakingMoneyATM.java
+++ LAB1/src/MakingMoneyATM.java
@@ -3,7 +3,7 @@ public class MakingMoneyATM {
 	public static void main(String[] args) {
 		System.out.println("*	*	*	*	*	*	*	*	*	*	*	*	*	*	*");
 		System.out.println("*														*");
-		System.out.println("*				Making Money Bank ATM Service			*");
+		System.out.println("*						Making Money Bank ATM Service					*");
 		System.out.println("*														*");
 		System.out.println("*	*	*	*	*	*	*	*	*	*	*	*	*	*	*");
 		System.out.println("");
